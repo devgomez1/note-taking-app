905abdaa73b388e2986956aa36d1cd7159bd0975
devin <devinelliotgomez@gmail.com>
2023-02-23 20:06:14 -0600

    changed some things like the port number and html routes, was having some problems

--- routes/index.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-const apiRoutes = require("./api");
-const app = express();
-app.use("/api", apiRoutes);
-module.exports = app;
--- server.js
+++ server.js
@@ -1,24 +1,26 @@
-const express = require("express");
-const path = require("path");
-const fs = require("fs");
+const express = require('express');
+const apiRoutes = require('./routes/api');
+const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // Middleware to parse JSON in request body
 app.use(express.json());
 
 // Serve static assets from the public folder
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-// GET /notes route to return the notes.html file
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "notes.html"));
+// Use API routes middleware
+app.use('/api', apiRoutes);
+
+// HTML routes
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
 
-// GET * route to return the index.html file
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Start the server
